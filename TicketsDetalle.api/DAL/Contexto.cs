4fcca2279f37cb4ea2675d5288f95d5e1119fbda
Stiinger <ortegantony29@gmail.com>
2024-02-26 08:28:10 -0400

    Crear contexto.

---
+++ TicketsDetalle.api/DAL/Contexto.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Library.Models;
+
+namespace TicketsDetalle.api.DAL
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+        public DbSet<Tickets> Tickets { get; set; }
+        public DbSet<DetalleTickets> DetalleTickets { get; set; }
+        public DbSet<Clientes> Clientes { get; set; } = default!;
+    }
+}
